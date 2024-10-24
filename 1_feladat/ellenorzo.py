2ae432755979a749c20ef5f28c2e5b55f03ccea1
Lydiagyenes <145264775+Lydiagyenes@users.noreply.github.com>
2024-10-24 10:31:47 +0200

    Update ellenorzo.py

--- 1_feladat/ellenorzo.py
+++ 1_feladat/ellenorzo.py
@@ -1,1 +1,29 @@
+#!/usr/bin/env python3
 
+def icecreamParlor(m, arr):
+    cost_map = {}
+    for i, cost in enumerate(arr):
+        diff = m - cost
+        if diff in cost_map:
+            return [cost_map[diff] + 1, i + 1]
+        cost_map[cost] = i
+
+if __name__ == '__main__':
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    idx = 0
+    t = int(data[idx])
+    idx += 1
+    results = []
+    for _ in range(t):
+        m = int(data[idx])
+        idx += 1
+        n = int(data[idx])
+        idx += 1
+        arr = list(map(int, data[idx:idx+n]))
+        idx += n
+        result = icecreamParlor(m, arr)
+        results.append(" ".join(map(str, result)))
+    
+    print("\n".join(results))
